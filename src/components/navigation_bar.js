b426ec3c747129eb145eb065d9ca741ffa8b6c90
lazaridoue <lazaridou.eleni00@gmail.com>
2020-12-28 21:00:35 -0500

    Organized components folder

--- src/components/navigation_bar.js
+++ src/components/navigation_bar.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
-function Navbar() {
-    return (
-        <>
-        <nav className="navbar">
-            <div className="navbar-container">  
-            </div>
-        </nav>
-        </>
-    )
-}
-
-export default Navbar
